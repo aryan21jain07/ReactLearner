c3ebdaa454947c2aefc5b9cd61eddcb3dcea16f8
aryan21jain07 <aryan21jain07@gmail.com>
2025-11-07 09:23:06 +0530

    props completed

---
+++ 03propsTailwindProject/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  let myObj={
+    name:"aryan",
+    age:22    ,
+    channel:"code with aryan",
+
+  }
+
+  let newArray=[1,2,3,4,5,6,7,8,9];
+  return (
+    <>
+      <h1 className ='bg-green-400 text-black p-4 rounded-xl' > tailwind test</h1>
+    
+   <Card username ="aryan" btnText = "click me " />
+   <Card username ="vikas" btnText ="visit me" />
+   
+
+    </>
+  )
+}
+
+export default App
